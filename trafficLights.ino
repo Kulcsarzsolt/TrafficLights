edc8f5fe3bdb8acf575a2c3c9a79156052f57fbf
Kulcsarzsolt <kulcsarzsolt15@gmail.com>
2020-07-30 16:01:24 +0300

    7 segment display - display() impl.

--- trafficLights.ino
+++ trafficLights.ino
@@ -12,10 +12,88 @@
 #define DISPLAY_G 5
 #define DISPLAY_DP 6
 
+void resetDisplay() {
+  digitalWrite(DISPLAY_A, LOW);
+  digitalWrite(DISPLAY_B, LOW);
+  digitalWrite(DISPLAY_C, LOW);
+  digitalWrite(DISPLAY_D, LOW);
+  digitalWrite(DISPLAY_E, LOW);
+  digitalWrite(DISPLAY_F, LOW);
+  digitalWrite(DISPLAY_G, LOW);
+  digitalWrite(DISPLAY_DP, LOW);
+}
+
+void turnOffDisplay() {
+  digitalWrite(DISPLAY_A, HIGH);
+  digitalWrite(DISPLAY_B, HIGH);
+  digitalWrite(DISPLAY_C, HIGH);
+  digitalWrite(DISPLAY_D, HIGH);
+  digitalWrite(DISPLAY_E, HIGH);
+  digitalWrite(DISPLAY_F, HIGH);
+  digitalWrite(DISPLAY_G, HIGH);
+  digitalWrite(DISPLAY_DP, HIGH);
+}
+
+void display(int input) {
+  switch (input) {
+  case 0:
+    digitalWrite(DISPLAY_G, HIGH);
+    break;
+  case 1:
+    digitalWrite(DISPLAY_A, HIGH);
+    digitalWrite(DISPLAY_D, HIGH);
+    digitalWrite(DISPLAY_E, HIGH);
+    digitalWrite(DISPLAY_F, HIGH);
+    digitalWrite(DISPLAY_G, HIGH);
+    break;
+  case 2:
+    digitalWrite(DISPLAY_C, HIGH);
+    digitalWrite(DISPLAY_F, HIGH);
+    break;
+  case 3:
+    digitalWrite(DISPLAY_E, HIGH);
+    digitalWrite(DISPLAY_F, HIGH);
+    break;
+  case 4:
+    digitalWrite(DISPLAY_A, HIGH);
+    digitalWrite(DISPLAY_D, HIGH);
+    digitalWrite(DISPLAY_E, HIGH);
+    break;
+  case 5:
+    digitalWrite(DISPLAY_B, HIGH);
+    digitalWrite(DISPLAY_E, HIGH);
+    break;
+  case 6:
+    digitalWrite(DISPLAY_B, HIGH);
+    break;
+  case 7:
+    digitalWrite(DISPLAY_D, HIGH);
+    digitalWrite(DISPLAY_E, HIGH);
+    digitalWrite(DISPLAY_F, HIGH);
+    digitalWrite(DISPLAY_G, HIGH);
+    break;
+  case 8:
+    break;
+  case 9:
+    digitalWrite(DISPLAY_E, HIGH);
+    break;
+  default:
+    digitalWrite(DISPLAY_B, HIGH);
+    digitalWrite(DISPLAY_C, HIGH);
+  }
+}
+
 void runFirstState() {
+
   digitalWrite(FIRST_LED, HIGH);
-  delay(500);
+  for (int i = 9; i >= 0; i--) {
+    resetDisplay();
+    display(i);
+    delay(1000);
+
+  }
   digitalWrite(FIRST_LED, LOW);
+  turnOffDisplay();
 }
 
 
@@ -33,8 +111,13 @@ void runSecondState() {
 
 void runThirdState() {
   digitalWrite(THIRD_LED, HIGH);
-  delay(500);
+  for (int i = 5; i >= 0; i--) {
+    resetDisplay();
+    display(i);
+    delay(1000);
+  }
   digitalWrite(THIRD_LED, LOW);
+  turnOffDisplay();
 
 }
 
@@ -47,6 +130,7 @@ void (*lightStates[3])() = {
 
 boolean direction = true;
 int currentState = 1;
+
 
 void setup() {
   pinMode(FIRST_LED, OUTPUT);
